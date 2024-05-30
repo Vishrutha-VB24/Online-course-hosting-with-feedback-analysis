548cc449047305e7f3286e5307ececb66e5abf8a
Vishrutha V B <Vishruthavb24@gmail.com>
2024-05-30 16:24:10 +0530

    Updated syntax of instructor.models.js

--- src/models/instructor.models.js
+++ src/models/instructor.models.js
@@ -1,13 +1,31 @@
 import mongoose from "mongoose"
 const instructorSchema = new mongoose.Schema(
     {
-        name: { type:String, required:true},
-        userName: {type:String, required:true},
-        email: {type:String, required:true},
-        password: {type:String, required:true},
-        phoneNumber: { type:String, required:true},
-        bio: { type:String, required:true}, //specification, qualification, experience
-    }, {timestamps: true}
+        name: {
+            type: String,
+            required: true
+        },
+        userName: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        phoneNumber: {
+            type: String,
+            required: true
+        },
+        bio: {
+            type: String,
+            required: true
+        }, //specification, qualification, experience
+    }, { timestamps: true }
 )
 export const Instructor = mongoose.model("Instructor", instructorSchema)
 
