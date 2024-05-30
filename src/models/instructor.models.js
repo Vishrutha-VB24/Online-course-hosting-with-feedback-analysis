3004c4649b4b15ea1eeb891abd33e0b195071557
RashmithaR26 <RashmithaR26.rashmithar876@gmail.com>
2024-05-30 15:45:31 +0530

    instructor model

--- src/models/instructor.models.js
+++ src/models/instructor.models.js
@@ -2,12 +2,11 @@ import mongoose from "mongoose"
 const instructorSchema = new mongoose.Schema(
     {
         instructor_name: { type:String, required:true},
+        instructor_id: {type:String, required:true},
         email: {type:String, required:true},
         password: {type:String, required:true},
         phone_number: { type:String },
-        specialization: { type:String },
-        qualification: { type:String },
-        experience: { type:String },
+        bio: { type:String }, //specification, qualification, experience
         courses: {type:[String] },
 
         current_course:{ type:String},
