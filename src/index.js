4236d6cb97b7258b92c536f94e364051699b09ac
Vishrutha V B <Vishruthavb24@gmail.com>
2024-06-25 19:06:26 +0530

    utilis done

--- src/index.js
+++ src/index.js
@@ -6,3 +6,11 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Sever is running at port : $ {process.env.PORT}`)
+    })//server start now till then mondb connected
+})
+.catch((err)=>{
+    console.log("MONGO db connection failed !!!",err);
+})
