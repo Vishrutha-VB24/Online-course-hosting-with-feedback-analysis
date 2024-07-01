51af85410dfd4f555a588718fda30cc8e1856d01
Vishrutha V B <Vishruthavb24@gmail.com>
2024-07-01 21:40:04 +0530

    small update

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,7 @@
 const asynHandler = (requestHandler) =>{
-   (req,res,next)=>{
+   return (req,res,next)=>{
     Promise.resolve(requestHandler(req, res, next)). catch((err)=>next(err))
-   }
+   } 
 }
 
 
