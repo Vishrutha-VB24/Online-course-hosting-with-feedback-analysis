9abc83b3548a0581cc0b5a4d4626f724793faa8d
srujan-j24 <srujanj2021@gmail.com>
2024-07-08 22:14:12 +0530

    api updated

--- frontend/src/utils/apis.js
+++ frontend/src/utils/apis.js
@@ -12,19 +12,33 @@ function register(data, role){
 function getUser(role){
     return axios.get(`${apiRoute}/${role}/current`);
 }
-const logout = (role) => {
+function logout(role){
     return axios.get(`${apiRoute}/${role}/logout`);
 }
 
-const getAllCourse = () =>{
-    return axios.get("http://localhost:8000/api/course/all");
+function getAllCourse(){
+    return axios.get(`${apiRoute}/course/all`);
     //should return an array of posts where each post contains tumbnail_url, title, descripton
 }
 
-const getCourse = (courseID) =>{
-    return axios.get(`http://localhost:8000/api/course/${courseId}`);
+function getCourse(courseID){
+    return axios.get(`${apiRoute}/course/${courseID}`);
+    //should return a course object of reuested is and with variable indicating the user has register to that course
 }
 
+function courseRegistration(courseID){
+    return axios.get(`${apiRoute}/course/register/${courseID}`);
+}
+
+function getAllCourseVideos(courseID){
+    return axios.get(`${apiRoute}/course/${courseID}/videos`);
+    //should return an array of video objects of the mentioned course if the user is registered to the course
+}
+
+function getCourseVideo(courseID, videoID){
+    return axios.get(`${apiRoute}/course/${courseID}/video/${videoID}`)
+    //should return a single video object of mentioned id if the user is registered to the course
+}
 
 
 export {
