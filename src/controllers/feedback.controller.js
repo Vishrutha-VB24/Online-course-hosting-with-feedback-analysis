3b304b4626da4c5b03fa30eeb7faff57f0038c9b
RashmithaR26 <RashmithaR26.rashmithar876@gmail.com>
2024-07-12 18:32:18 +0530

    feedback

--- src/controllers/feedback.controller.js
+++ src/controllers/feedback.controller.js
@@ -6,15 +6,15 @@ import axios from "axios";
 const newfeedback = asyncHandler(async (req, res) => {
     const { courseID } = req.params;
     const {feedback} = req.body;
-    // if (!req.student) {
-    //     throw new ApiError(401, "Unauthorized student");
-    // }
-
+    if (!req.student) {
+        throw new ApiError(401, "Unauthorized student");
+    }
+        
     try {
         const response = await axios.post("http://127.0.0.1:8000/api/reviews/", {"review": feedback});
 
-        // Handle the response from the Django server
-        res.status(response.status).json(new ApiResponse(response.data));
+        console.log(response.data.predicted_rating)
+        
     } catch (error) {
         // Handle error from the request
         throw new ApiError(error.response ? error.response.status : 500, error.message);
